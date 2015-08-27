40367c3ff26ac265fa9d51b4942e92cf854345dd
Danny <dannywestlake@ascotproducts.com>
2015-08-27 09:22:10 -0500

    test commit
    test commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	</style>
     <div id="main-wrapper-index">
    <div id="writing"> <h1 class="homepage-font">For over half a century, Robeco has been a leader in supplying plastics to customers who seek superior quality, excellent customer service, and competitive prices.</h1>
-       <br><br> <h1 class="homepage-font">Our warehouses around the U.S. are ready to ship your order.</h1></div>
+       <br><br> <h1 class="homepage-font">Our warehouses around the U.S. are ready to ship your order.</h1> </div>
 </div>
 
  <?php include("INCLUDE/footer.php"); ?>
